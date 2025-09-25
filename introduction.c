440309b6777c8815df97ee50d6ec6da6f948a012
Dennis Rence <denniskrence@gmail.com>
2025-09-25 11:03:11 +0300

    Create introduction.c

---
+++ introduction.c
@@ -0,0 +1,16 @@
+// Simple C program (Introduction)
+/*
+Name:Dennis Kariuki
+Reg No: CT101/G/26473/25
+*/
+#include <stdio.h>
+//main Function
+int main (){
+    printf ("Name :Am Dennis Rence \n");
+    printf("Reg Number : CT101/G/26473/25 \n");
+     printf("School :KyU Student \n");
+     printf("Course :Computer Science\n");
+     printf("\n");
+     printf("program by rence");
+    return 0;	
+}
