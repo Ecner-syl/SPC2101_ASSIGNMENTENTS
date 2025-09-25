0948eddd56d5e113471a10872001b628b0ef0bfb
Dennis Rence <denniskrence@gmail.com>
2025-09-25 11:05:29 +0300

    Create units display.c

---
+++ units display.c
@@ -0,0 +1,19 @@
+//  Computer science Semester 1 Units
+/*
+Name:Dennis Kariuki
+Reg No: CT101/G/26473/25
+*/
+#include <stdio.h>
+//main Function
+int main (){
+    printf("     1.1 Computer Units \n");
+    printf(" SPC2101: INTRODUCTION TO PROGRAMMING AND PROBLEM SOLVING\n");
+    printf(" UCU]]]02101:COMMUNICATION SKILLS \n");
+    printf(" UCU2105: FUNDAMENTALS OF ICT\n");
+    printf(" SPH2101: PHYSICS\n");
+    printf(" SPC2102: INTRODUCTION TO COMPUTER ARCHITECHTURE\n");
+    printf(" SPH2107: ANALOGUE ELECTRONICS\n");
+    printf(" SPM2100: DISCRETE MATH3MATICS\n");
+    printf(" SPM2104: MATHEMATICS FOR SCIENCE\n");
+    return 0;	
+}
